ad2d6e0a6ca9700ac63cf12393e599291e23347e
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-05 19:52:30 +0545

    configure packages

--- server/index.js
+++ server/index.js
@@ -30,3 +30,13 @@ app.listen(port, () => {
 
 //Connect database
 connectDB();
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}

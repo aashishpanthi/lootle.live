3eddf00280bd3b36bf9cb35486738402f2eb21c3
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-07 22:52:05 +0545

    Servce the frontend on production

--- server/index.js
+++ server/index.js
@@ -34,9 +34,9 @@ connectDB();
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(resolve("../client/build/index.html"));
   });
 }

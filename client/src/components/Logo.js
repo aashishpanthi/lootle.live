6eb6f781c7bb5cbd4a69996968294bea37c790ea
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-07 22:16:15 +0545

    remove text decoration

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom";
 
 const Logo = ({ name }) => {
   return (
-    <Link to="/" className="brand">
+    <Link
+      to="/"
+      className="brand"
+      style={{
+        textDecoration: "none",
+      }}
+    >
       <img src="/logo192.png" alt="Lootle logo" />
       {name && (
         <Typography variant="h5" component="h1" className="brand-name">

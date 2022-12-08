37d72c4535b016dbeef958b91fac4ec6db11e4c1
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-08 20:21:21 +0545

    PWA

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,3 +13,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// PWA
+serviceWorkerRegistration.register();

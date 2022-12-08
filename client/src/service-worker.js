b51f63139464b80f1e3067a396700cb29c4e5527
Aashish Panthi <aashishpanthi11@gmail.com>
2022-12-08 20:55:59 +0545

    solve self error

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -6,7 +6,7 @@ import { StaleWhileRevalidate } from "workbox-strategies";
 
 clientsClaim();
 
-precacheAndRoute(self.__WB_MANIFEST);
+precacheAndRoute(WorkerGlobalScope.self.__WB_MANIFEST);
 
 const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
 registerRoute(
@@ -35,7 +35,8 @@ registerRoute(
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) =>
-    url.origin === self.location.origin && url.pathname.endsWith(".png"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+    url.origin === WorkerGlobalScope.self.location.origin &&
+    url.pathname.endsWith(".png"), // Customize this strategy as needed, e.g., by changing to CacheFirst.
   new StaleWhileRevalidate({
     cacheName: "images",
     plugins: [
@@ -48,8 +49,8 @@ registerRoute(
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
-self.addEventListener("message", (event) => {
+WorkerGlobalScope.self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
+    WorkerGlobalScope.self.skipWaiting();
   }
 });
